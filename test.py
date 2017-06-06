412cef0323dec6854dcbdad591fc92cdebbbea19
Cedric BARBIER <cbarbier@e2r5p12.42.fr>
2017-06-06 18:59:09 +0200

    starting to set the visu

---
+++ test.py
@@ -0,0 +1,23 @@
+import sys
+import Tkinter as tk
+
+def get_ants():
+    line = sys.stdin.readline()  
+    try:
+        nb = int(line)
+    except ValueError:
+        print "number of ant is not an integer"
+        sys.exit()
+
+def get_nodes():
+    nodes = {}
+    for line in sys.stdin:
+        if line == "##start"
+            if line = sys.stdin
+        print line
+
+get_ants()
+#app = tk.Tk()
+#app.title("test")
+#app.grid()
+#app.mainloop()
